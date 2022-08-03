30bf7047e650fafd03fb2ac8b10a20b6e9d2ab90
Christian Collins <christiancollins743@gmail.com>
2022-08-03 15:33:06 -0700

    First Commit

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu, ipcMain, Tray, dialog} = require('electron')
+const electronReload = require('electron-reload')
+const url = require('url')
 const path = require('path')
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Kettlebell1601!',
+  database: 'copy_cat_app'
+})
 
+
+
+let tray = null;
 function createWindow () {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+   mainWindow = new BrowserWindow({
+    width: 300,
+    height: 500,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -14,24 +26,180 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+  Menu.setApplicationMenu(mainMenu)
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
+  const iconPath = path.join(__dirname, '3x/Asset 3@3x.png')
+  console.log(iconPath);
+  tray = new Tray(iconPath);
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio', checked: true },
+    { label: 'Item2', type: 'radio'},
+    { label: 'Item3', type: 'radio'},
+    { label: 'Item4', type: 'radio'},
+  ])
+  tray.setToolTip('This is my app');
+  tray.setContextMenu(contextMenu);
 }
+// Load DB Items into favorites
+function loadFavorites(){
+  connection.connect(function(err){
+    if(err) throw err;
+    let sql = 'SELECT * FROM items_copied'
+    connection.query(sql, function(err, result){
+      if(err) throw err;
+      console.log(result);
+      mainWindow.webContents.send('loadFavorites', result);
+    })
+  })
+}
+//Catch item:add
+ipcMain.on('item:add', function(e, item){
+  console.log(item);
+  mainWindow.webContents.send('item:add', item);
+  addWindow.close();
+})
+// Read Clipboard add
+ipcMain.on('text:copied', function(e, text){
+  // console.log(text);
+  mainWindow.webContents.send('text:copied', text);
+})
+// Add Favorite Item to database
+ipcMain.on('item:add-to-db', function(e, text){
+  connection.connect(function(err){
+    if (err) throw err;
+    let sql = `INSERT INTO items_copied(item_copied) VALUES ('${text}')`;
+    connection.query(sql, function(err, result){
+      if(err) throw err;
+      console.log('1 record inserted')
+    })
+    mainWindow.webContents.send('item:add-to-favorites', text)
+  });
+})
+//Delete Item from DB
+ipcMain.on('item:delete-from-db', function(e, text){
+  let options = {
+    buttons: ["Yes", "Nope", "Cancel"],
+    message: "Are you sure you want to delete this item?"
+  }
 
+  const response = dialog.showMessageBoxSync(mainWindow,options);
+  console.log(response);
+  console.log(text);
+  if (response == 0 ){
+    mainWindow.webContents.send('item:deleted', text);
+    connection.connect(function(err){
+      let query = typeof text === 'string' ? text : text.item_copied;
+      if (err) throw err;
+      let sql = `DELETE FROM items_copied WHERE item_copied ='${query}'`;
+      connection.query(sql, function(err, result){
+        if(err) throw err;
+        console.log(`1 record deleted ${query}`)
+      })
+    });
+  }
+})
+//Declare addWindow and mainWindow
+let addWindow;
+let mainWindow;
+// Handle create add windows
+function createAddWinow(){
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    // contextIsolation: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'newWindow.html' ),
+    protocol: 'file',
+    slashes: true
+  }))
+  addWindow.on('close', function(){
+    addWindow = null;
+  })
+}
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add Item',
+        accelerator: process.platform == 'darwin' ? 'Command+N' : 'ctrl+N',
+        click(){
+          createAddWinow();
+        }
+      },
+      {
+        label: 'Clear Items'
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'crtl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Copy',
+      },
+      {
+        label: 'Pasta'
+      }
+    ]
+  }
+]
+//if mac add empty object to Menu
+if(process.platform == 'darwin'){
+  mainMenuTemplate.unshift({
+    label:"cool"
+  })
+}
+//Add developer DevTools
+if(process.env.NODE_ENV != 'production'){
+  mainMenuTemplate.push({
+    label: 'Dev Tools',
+    submenu: [
+      {
+        label: 'Toggle Tools',
+        accelerator: process.platform == 'darwin' ? 'Command+I' : 'crtl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  })
+}
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
+
   createWindow()
-  
+  mainWindow.once('ready-to-show', () => {
+    loadFavorites();
+  })
+
+  mainWindow.on('closed', function(){
+    app.quit();
+  })
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
-
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
--- preload.js
+++ preload.js
@@ -10,3 +10,178 @@ window.addEventListener('DOMContentLoaded', () => {
     replaceText(`${type}-version`, process.versions[type])
   }
 })
+console.log('hello from the preload script')
+const electron = require('electron');
+const {ipcRenderer, clipboard, contextBridge} = electron;
+const clipBoardListener = require('electron-clipboard-extended');
+contextBridge.exposeInMainWorld("api", {
+  submitIt:(item) => {
+      ipcRenderer.send('item:add', item);
+  },
+  listenForIt:(item) => {
+    ipcRenderer.on('item:add', function(e, item){
+      const ul = document.querySelector('#todo');
+      const li = document.createElement('li');
+      const itemText = document.createTextNode(item);
+      li.appendChild(itemText);
+      ul.appendChild(li);
+    })
+  },
+  clipIt: () => {
+    clipBoardListener.on('text-changed', () => {
+      canListenForCopy = true;
+      let text = clipBoardListener.readText();
+      ipcRenderer.send('text:copied', text);
+    }).startWatching()
+  },
+  turnOffClip: () => {
+    canListenForCopy = false;
+    clipBoardListener.off('text-changed');
+  },
+  listenForCopy:() => {
+// Checks to see if item is already on clipboard and if not adds item to UI
+    ipcRenderer.on('text:copied', function(e, text){
+      if (textOnClipBoard.includes(text)){
+        console.log('text already copied brah');
+      }else{
+        const ul = document.querySelector('#clipboard');
+        const li = document.createElement('li');
+        const favIcon = document.createElement('span');
+        const displayCopiedText = document.createElement('span');
+        const copyItemText = document.createTextNode(text);
+        displayCopiedText.classList.add('regular-copied-item')
+        displayCopiedText.appendChild(copyItemText);
+        li.appendChild(displayCopiedText);
+        // li.classList.add('copied-list-item','collection-item','changeit');
+        li.classList.add('copied-list-item','collection-item');
+        favIcon.classList.add('secondary-content');
+        favIcon.innerHTML += '<i class="material-icons">favorite_border</i>'
+        li.appendChild(favIcon);
+        displayCopiedText.addEventListener('click',(e) => {
+          clipBoardListener.writeText(displayCopiedText.innerText);
+          new Notification('Item Copied',{body: `You Just Copied \"${displayCopiedText.innerText}\"`})
+        })
+        favIcon.addEventListener('click', (e) => {
+          ipcRenderer.send('item:add-to-db', text);
+          favIcon.innerHTML = '<i class="material-icons">favorite</i>'
+          new Notification('Item Added To Favorites', {body:`${text} added to favorites`})
+          console.log('item submitted to database');
+        })
+        // canListenForCopy ? li.classList.add('changeit', 'font-engaged') : li.classList.add('changeit');
+        ul.appendChild(li);
+       textOnClipBoard.push(text);
+      }
+    })
+  },
+  addToFavorites: () => {
+    ipcRenderer.on('item:add-to-favorites', function(e, text){
+
+        const favorites = document.getElementById('favorites');
+        const li = document.createElement('li');
+        const favIcon2 = document.createElement('span');
+        const displayCopiedText = document.createElement('span');
+        const copyItemText = document.createTextNode(text);
+        displayCopiedText.classList.add('favorite');
+        displayCopiedText.appendChild(copyItemText);
+        li.appendChild(displayCopiedText);
+        // li.classList.add('copied-list-item','collection-item','changeit');
+        li.classList.add('copied-list-item','collection-item', 'favorite-list-item');
+        favIcon2.classList.add('secondary-content');
+        favIcon2.innerHTML += '<i class="material-icons">favorite</i>'
+        li.appendChild(favIcon2);
+        displayCopiedText.addEventListener('click',(e) => {
+          clipBoardListener.writeText(displayCopiedText.innerText);
+          new Notification('Item Copied',{body: `You Just Copied \"${displayCopiedText.innerText}\"`})
+        })
+        favIcon2.addEventListener('click', (e) => {
+          ipcRenderer.send('item:delete-from-db', text);
+          favIcon2.innerHTML = '<i class="material-icons">favorite_border</i>'
+          console.log('delete request submitted to database');
+        })
+        // canListenForCopy ? li.classList.add('changeit', 'font-engaged') : li.classList.add('changeit');
+        favorites.appendChild(li);
+       textOnClipBoard.push(text);
+
+    })
+  },
+  deleteRenderFromFavorites: () => {
+    ipcRenderer.on('item:deleted', function(e, text){
+      let spansToSearch = document.querySelectorAll('.favorite');
+      let listItemToDelete = document.querySelectorAll('.favorite-list-item');
+      let favoritesUl = document.querySelector('#favorites');
+      let searchText = typeof text === 'string'? text : text.item_copied;
+      let recentlyCopied = document.querySelectorAll('.regular-copied-item');
+      let favoriteIcon = document.querySelectorAll('.material-icons');
+
+      for(let i = 0; i < spansToSearch.length; i++){
+        if(spansToSearch[i].textContent == searchText){
+          listItemToDelete[i].parentNode.removeChild(listItemToDelete[i])
+
+        } fart
+      }
+      for(let i = 0; i < recentlyCopied.length; i++){
+        if(recentlyCopied[i].textContent == searchText){
+          favoriteIcon[i].innerText = 'favorite_border'
+        }
+      }
+    })
+  },
+  loadFavorites: () => {
+     ipcRenderer.on('loadFavorites', function(e, results){
+       const favorites = document.getElementById('favorites');
+
+      results.forEach(text => {
+        console.log(text)
+        const li = document.createElement('li');
+        const favIcon2 = document.createElement('span');
+        const displayCopiedText = document.createElement('span');
+        const copyItemText = document.createTextNode(text.item_copied);
+        displayCopiedText.classList.add('favorite');
+        displayCopiedText.appendChild(copyItemText);
+        li.appendChild(displayCopiedText);
+        // li.classList.add('copied-list-item','collection-item','changeit');
+        li.classList.add('copied-list-item','collection-item', 'favorite-list-item');
+        favIcon2.classList.add('secondary-content');
+        favIcon2.innerHTML += '<i class="material-icons">favorite</i>'
+        li.appendChild(favIcon2);
+        displayCopiedText.addEventListener('click',(e) => {
+          clipBoardListener.writeText(displayCopiedText.innerText);
+          new Notification('Item Copied',{body: `You Just Copied \"${displayCopiedText.innerText}\"`})
+        })
+        favIcon2.addEventListener('click', (e) => {
+          ipcRenderer.send('item:delete-from-db', text);
+          favIcon2.innerHTML = '<i class="material-icons">favorite_border</i>'
+          console.log('delete request submitted to database');
+        })
+        // canListenForCopy ? li.classList.add('changeit', 'font-engaged') : li.classList.add('changeit');
+        favorites.appendChild(li);
+      })
+
+    })
+  }
+})
+let canListenForCopy = false;
+let textOnClipBoard = []
+// const {
+//   contextBridge,
+//   ipcRenderer
+// } = require('electron');
+//
+// contextBridge.exposeInMainWorld(
+//     "api", {
+//         send: (channel, data) => {
+//             // whitelist channels
+//             let validChannels = ["toMain"];
+//             if (validChannels.includes(channel)) {
+//                 ipcRenderer.send(channel, data);
+//             }
+//         },
+//         receive: (channel, func) => {
+//             let validChannels = ["fromMain"];
+//             if (validChannels.includes(channel)) {
+//                 // Deliberately strip event as it includes `sender`
+//                 ipcRenderer.on(channel, (event, ...args) => func(...args));
+//             }
+//         }
+//     }
+// );
